b3795a67d39d8baf216aa09f83b88e28ece4da96
Kumargit20 <kumar.kumaraveluwork@gmail.com>
2025-05-05 17:13:01 +0530

    Apppro

---
+++ src/org/program/Program_001.java
@@ -0,0 +1,22 @@
+package org.program;
+
+public class Program_001 {
+
+	public static void main(String[] args) {
+		String s="welcome to java";
+		String[] split = s.split(" ");
+		for(String word:split) {
+			char charAt = word.charAt(0);
+			char upperCase = Character.toUpperCase(charAt);
+			String substring = word.substring(1);
+			String output = upperCase+substring;
+			System.out.println(output);
+			
+		}
+			
+			
+		
+		}
+	
+}
+
